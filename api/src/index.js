6d7440f57f3a0e0ba5bbc688256f886331647739
Cody Haines <chaines51@gmail.com>
2021-04-05 13:44:10 -0500

    Merged <_<

--- api/src/index.js
+++ api/src/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
+const Router = require('./routes');
 
-app.use('/', (req, res) => {
-  res.send('Hello, Back-End!');
-});
+app.use('/', Router);
 
 app.listen(process.env.PORT, () => {
   console.log(`Express app listening on port ${process.env.PORT}`);
