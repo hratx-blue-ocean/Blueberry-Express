68e2b29a1a1bbf81867c6c25cf61c46ebdb06389
Zach Thomas <zach.thomas2401@gmail.com>
2021-04-05 14:57:49 -0500

    PreviousBtn

--- react/src/components/App.jsx
+++ react/src/components/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import PreviousBtn from './Buttons/PreviousBtn'
 
 const App = () => (
   <div>
     <h1 className="text-3xl font-extrabold">Hello, Front-End!</h1>
     process.env.EXAMPLE_VARIABLE: {process.env.EXAMPLE_VARIABLE}
+    <PreviousBtn/>
   </div>
 );
 
---
+++ react/src/components/Buttons/PreviousBtn.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const PreviousBtn = ({handleClick}) => {
+  return (
+    <button
+    type="button"
+    className="h-10 px-5 m-2 text-white transition-colors duration-150 bg-blue-600 rounded-lg focus:shadow-outline hover:bg-blue-800"
+    onClick={handleClick}>
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+      </svg>
+    </button>
+  )
+}
+
+export default PreviousBtn;
+
+
