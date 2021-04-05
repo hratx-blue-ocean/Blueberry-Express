43a1706f8428d70f13b005dd528f27bf5f25d466
Brandon Harden <bmh0013@gmail.com>
2021-04-05 11:59:05 -0500

    Nav component created

---
+++ react/src/components/Shared/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const Nav = () => (
+  <div className="inline-block">
+    <ul>
+      <li className="text-blue-600 inline">
+        Dashboard
+      </li>
+      <li className="text-blue-600 inline ml-5">
+        Calendar
+      </li>
+      <li className="text-blue-600 inline ml-5">
+        Profile
+      </li>
+    </ul>
+  </div>
+);
+
