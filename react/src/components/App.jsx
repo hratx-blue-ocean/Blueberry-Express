3c55ddc48106be0d9072e60abcf2ff5c558f7e81
Zach Thomas <zach.thomas2401@gmail.com>
2021-04-05 14:59:07 -0500

    Continue Button

--- react/src/components/App.jsx
+++ react/src/components/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import PreviousBtn from './Buttons/PreviousBtn'
+import ContinueBtn from './Buttons/ContinueBtn'
 
 const App = () => (
   <div>
     <h1 className="text-3xl font-extrabold">Hello, Front-End!</h1>
     process.env.EXAMPLE_VARIABLE: {process.env.EXAMPLE_VARIABLE}
-    <PreviousBtn/>
+    <ContinueBtn/>
   </div>
 );
 
---
+++ react/src/components/Buttons/ContinueBtn.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const ContinueBtn = ({handleClick}) => {
+  return (
+    <button
+    type="button"
+    className="h-10 px-5 m-2 text-white transition-colors duration-150 bg-blue-600 rounded-lg focus:shadow-outline hover:bg-blue-800"
+    onClick={handleClick}>
+<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+</svg>
+    </button>
+  )
+}
+
+export default ContinueBtn;
