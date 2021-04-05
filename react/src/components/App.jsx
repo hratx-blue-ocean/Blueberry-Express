69e23cb7aab3bdd7707b867949dbfd39c251b56e
Brandon Harden <bmh0013@gmail.com>
2021-04-05 16:11:41 -0500

    Added react router for landing page and nav component

--- react/src/components/App.jsx
+++ react/src/components/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { LandingPage } from './Layouts/LandingPage.jsx';
+import { Switch, Route } from "react-router-dom";
 
 const App = () => (
   <div>
-    <h1 className="text-3xl font-extrabold">Hello, Front-End!</h1>
-    process.env.EXAMPLE_VARIABLE: {process.env.EXAMPLE_VARIABLE}
+    <Switch>
+      <Route path="/">
+        <LandingPage />
+      </Route>
+    </Switch>
   </div>
 );
 
