3a5318894b5ab4337f112d4f3669587fadf67895
Matt <matt.collins087@gmail.com>
2021-04-06 16:34:54 -0500

    Edited Landing Page/Added Site Font

--- react/src/components/Layouts/LandingPage.jsx
+++ react/src/components/Layouts/LandingPage.jsx
@@ -17,24 +17,21 @@ export const LandingPage = () => {
                 </div>
             </div>
 
-            <div className="w-3/4 mx-auto mt-12 border p-5 rounded-xl shadow-lg">
-                <div className="flex items-center justify-center mt-16">
-                    <div className="mr-16">
-                        <img src="assets/homepageLearn.png" alt="homepageLearn" className="h-96" />
-                    </div>
-                    <div className="flex flex-col items-end border p-5 rounded-xl shadow-md">
-                        <h1 className="text-3xl font-bold">Discover a long love for language.</h1>
-                        <div className="mt-7 mr-7">
-                            <LargeBtn label='Sign-up' />
-                        </div>
-                    </div>
+            <div className="flex items-center justify-center mt-16">
+                <div className="mr-16">
+                    <img src="assets/homepageLearn.png" alt="homepageLearn" className="h-96" />
                 </div>
-
-                <div className="flex flex-row items-center justify-around mt-20">
-                    <h3 className="text-lg italic text-gray-600">Connecting those who love to learn and those who love to teach.</h3>
+                <div className="flex flex-col items-end border p-5 rounded-xl shadow-md">
+                    <h1 className="text-3xl font-bold">Discover a long love for language.</h1>
+                    <div className="mt-7 mr-7">
+                        <LargeBtn label='Sign-up' />
+                    </div>
                 </div>
             </div>
 
+            <div className="flex flex-row items-center justify-around mt-20">
+                <h3 className="text-lg italic text-gray-600">Connecting those who love to learn and those who love to teach.</h3>
+            </div>
 
             <div className="fixed w-full bottom-0">
                 <Footer />
