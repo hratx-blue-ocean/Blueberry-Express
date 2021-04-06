598c9f5c0d2e143895e8efe422cb218be890c3b0
Zach Thomas <zach.thomas2401@gmail.com>
2021-04-06 15:19:49 -0500

    Student and teacher lang selection and TeachVLearn modals

--- react/src/components/Shared/LanguageSelection.jsx
+++ react/src/components/Shared/LanguageSelection.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export const LanguageSelection = ({ languages }) => {
 	return (
-		<div className="flex flex-col ml-5">
+		<div className="grid ml-5 grid-cols-3 p-10">
 			{languages.map((language) => (
 				<label className="inline-flex items-center mt-3">
-					<input type="radio" className="form-radio h-5 w-5 text-blue-600" />
+					<input type="checkbox" className="h-5 w-5 text-blue-600" />
 					<span className="ml-2 text-black">{language}</span>
 				</label>
 			))}
