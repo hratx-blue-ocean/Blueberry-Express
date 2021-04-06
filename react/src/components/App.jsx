78ddf3c61c6e72bf3ba4b15d54c024e876cda2b2
Brandon Harden <bmh0013@gmail.com>
2021-04-05 20:31:06 -0500

    config webpack to work with react router

--- react/src/components/App.jsx
+++ react/src/components/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { LandingPage } from './Layouts/LandingPage.jsx';
-import { Switch, Route } from "react-router-dom";
+import { Login } from './Layouts/Login.jsx';
+import { Signup } from './Layouts/Signup.jsx';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => (
-  <div>
-    <Switch>
-      <Route path="/">
-        <LandingPage />
-      </Route>
-    </Switch>
-  </div>
+  <Router>
+    <div>
+      <Switch>
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/signup" exact component={Signup} />
+      </Switch>
+    </div>
+  </Router>
 );
 
 export default App;
