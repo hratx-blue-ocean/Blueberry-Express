914fcfdcb07638815d9990298f7ee0bd6b16e879
Matt <matt.collins087@gmail.com>
2021-04-06 15:21:19 -0500

    Completed Student Home Componenet

--- react/src/components/App.jsx
+++ react/src/components/App.jsx
@@ -2,18 +2,16 @@ import React from 'react';
 import { LandingPage } from './Layouts/LandingPage.jsx';
 import { Login } from './Layouts/Login.jsx';
 import { Signup } from './Layouts/Signup.jsx';
-import { StudentHome } from './Layouts/StudentHome.jsx';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => (
   <Router>
     <div>
-      {/* <Switch>
+      <Switch>
         <Route path="/" exact component={LandingPage} />
         <Route path="/login" exact component={Login} />
         <Route path="/signup" exact component={Signup} />
-      </Switch> */}
-      <StudentHome/>
+      </Switch>
     </div>
   </Router>
 );
