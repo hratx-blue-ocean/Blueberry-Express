76e5fd7cd76c7e4b1d18676f9ed7994a9e10bb12
Matt <matt.collins087@gmail.com>
2021-04-06 09:02:37 -0500

    Edited App.jsx

--- react/src/components/App.jsx
+++ react/src/components/App.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import { LandingPage } from './Layouts/LandingPage.jsx';
+import { StudentHome } from './Layouts/StudentHome.jsx'
 import { Switch, Route } from "react-router-dom";
-
-import {StudentHome} from './Layouts/StudentHome.jsx'
 
 const App = () => (
   <div>
-    {/* <Switch>
+    <Switch>
       <Route path="/">
         <LandingPage />
       </Route>
-    </Switch> */}
-    <StudentHome/>
+    </Switch>
+
   </div>
 );
 
