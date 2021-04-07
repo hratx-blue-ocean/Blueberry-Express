05c551051156c6466a1933da9d47c6e590663eea
Cody Haines <chaines51@gmail.com>
2021-04-07 13:41:47 -0500

    Fixed typo

--- api/src/loggedIn.js
+++ api/src/loggedIn.js
@@ -8,10 +8,12 @@ const isLoggedIn = (req, res, next) => {
     try {
       const token = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
       db.user.findOne({ where: { googleKey: token.googleKey } }).then((user) => {
+        console.log('name: ', user.name);
+        console.log('isStudent: ', user.student);
         if (user.student === null) {
           req.userAuthorized = false;
         } else {
-          req.userAuthorize = true;
+          req.userAuthorized = true;
         }
         req.user = user;
         req.accessToken = token.accessToken;
