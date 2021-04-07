7d1711ec97d28884d3228b6466c9293e4e027972
Cody Haines <chaines51@gmail.com>
2021-04-07 08:00:42 -0500

    Added an effect to determine if the user is logged in on initial page load, and also an example 'context' hook, so that child components can access that information

--- react/src/components/App.jsx
+++ react/src/components/App.jsx
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { LandingPage } from './Layouts/LandingPage.jsx';
 import { Login } from './Layouts/Login.jsx';
 import { Signup } from './Layouts/Signup.jsx';
 import { StudentHome } from './Layouts/StudentHome.jsx';
 import { TeacherHome } from './Layouts/TeacherHome.jsx';
 import { UserProfile } from './Layouts/UserProfile.jsx';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import './appstyles.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './AppStyles.css';
+import { getUser } from '../api';
+import { AuthContext } from '../auth';
 
-const App = () => (
-  <Router>
-    <div>
-      <Switch>
-        <Route path="/" exact component={LandingPage} />
-        <Route path="/login" exact component={Login} />
-        <Route path="/signup" exact component={Signup} />
-        <Route path="/studenthome" exact component={StudentHome} />
-        <Route path="/teacherhome" exact component={TeacherHome} />
-        <Route path="/userprofile" exact component={UserProfile} />
-      </Switch>
-    </div>
-  </Router>
-);
+const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [userType, setUserType] = useState(null);
+  const [userName, setUserName] = useState('');
+  const contextValue = {
+    loggedIn,
+    userType,
+    userName,
+  };
+
+  useEffect(() => {
+    getUser().then((data) => {
+      console.log('data:', data);
+      if (data.id) {
+        setLoggedIn(true);
+        setUserType(data.type);
+        setUserName(data.displayName);
+      }
+    });
+  }, []);
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/" exact component={LandingPage} />
+            <Route path="/login" exact component={Login} />
+            <Route path="/signup" exact component={Signup} />
+            <Route path="/studenthome" exact component={StudentHome} />
+            <Route path="/teacherhome" exact component={TeacherHome} />
+            <Route path="/userprofile" exact component={UserProfile} />
+          </Switch>
+        </div>
+      </Router>
+    </AuthContext.Provider>
+  );
+};
 
 export default App;
