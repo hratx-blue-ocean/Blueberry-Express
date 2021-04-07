8fac7da7afe30865740bcb41d62228ccd17fd8bc
Tahsin Ahmed <tahsinahmed95@gmail.com>
2021-04-07 17:25:14 -0400

    added new teacher accept and reject buttons

--- react/src/components/TeacherOnly/TeacherAppointment.jsx
+++ react/src/components/TeacherOnly/TeacherAppointment.jsx
@@ -1,25 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MessageSend } from '../Modals/MessageSend.jsx';
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
 import CheckIcon from '@material-ui/icons/Check';
 import ClearIcon from '@material-ui/icons/Clear';
-import { LargeBtn } from '../Buttons/LargeBtn'
+import { LargeBtn } from '../Buttons/LargeBtn';
+
+const useStyles = makeStyles((theme) => ({
+  check: {
+    '& svg': {
+      fontSize: 35,
+      color: "white",
+      borderLeft: "1px solid white",
+      paddingLeft: "10px",
+    }
+  },
+  clear: {
+    '& svg': {
+      fontSize: 35,
+      color: "white",
+      paddingLeft: "10px",
+    }
+  },
+}));
 
 export const TeacherAppointment = ({ appointment }) => {
+  const[checked, setChecked] = useState(false);
+  const[clear, setClear] = useState(false);
+  const classes = useStyles();
 
   return (
     <div className="individual-appointment">
-      <div className="appointment-info">
-        <p className="pb-1 text-lg"><span>Student:</span> {appointment.with}</p>
-        <p className="pb-1"><span>Lang:</span> {appointment.lang}</p>
-        <p className="pb-1">4/5/21 {appointment.start}</p>
-      </div>
+      {clear ? 
+          <h1 className="text-3xl">Cleared</h1>
+        :
+        <>
+          <div className="appointment-info">
+            <p className="pb-1 text-lg"><span>Student:</span> {appointment.with}</p>
+            <p className="pb-1"><span>Lang:</span> {appointment.lang}</p>
+            <p className="pb-1">4/5/21 {appointment.start}</p>
+          </div>
 
-      <div className="flex appointment-button">
-        <MessageSend name={appointment.with}/>
-        <CheckIcon className="border-l"/>
-        <ClearIcon className="border-l"/>
-        {/* <LargeBtn label="Reschedule" /> */}
-      </div>
+          <div className="flex items-center appointment-button">
+            <MessageSend name={appointment.with}/>
+            {checked ?
+              <LargeBtn label="Reschedule" />
+            :
+              <>
+                <IconButton className={classes.check} onClick={() => {setChecked(true)}}>
+                  <CheckIcon />
+                </IconButton>
+                <IconButton className={classes.clear} onClick={() => {setClear(true)}}>
+                  <ClearIcon />
+                </IconButton>
+              </>
+            }
+          </div>
+        </>
+      }
+
     </div>
   )
 }
