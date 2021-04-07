bb344c5e410c75d2fd470a4e263a0ca87742045e
Cody Haines <chaines51@gmail.com>
2021-04-07 10:10:55 -0500

    Fixed an issue with how redirecting was handled

--- react/src/auth.js
+++ react/src/auth.js
@@ -2,17 +2,19 @@ import React from 'react';
 
 const setToken = (e) => {
     if(e.origin && e.data && e.data.command === 'token-ready' && e.data.info && e.data.info.token ) {
+        console.log('message recieved');
         localStorage.setItem('jwt', e.data.info.token);
-    }
-    e.source.postMessage(
-        {
-            command: 'info',
-            info: {
-                complete: true,
+        e.source.postMessage(
+            {
+                command: 'info',
+                info: {
+                    complete: true,
+                },
             },
-        },
-        e.origin
-    );
+            e.origin
+        );
+        window.location = '/';
+    }
 };
 
 
@@ -20,7 +22,7 @@ export const login = () => {
     console.log('Logging in');
     window.addEventListener('message', setToken, false);
     window.open('http://localhost:3000/auth/google');
-    window.location = '/';
+    //window.location = '/';
 }
 
 export const AuthContext = React.createContext();
