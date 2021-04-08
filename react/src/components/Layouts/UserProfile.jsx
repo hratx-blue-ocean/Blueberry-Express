6fadcab598521a795aa0e7a0a5eabfae0329caaf
Zach Thomas <zach.thomas2401@gmail.com>
2021-04-08 10:10:52 -0500

    Moved to conditional rendering in userprofile

--- react/src/components/Layouts/UserProfile.jsx
+++ react/src/components/Layouts/UserProfile.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import { TransparentLogo } from '../Shared/TransparentLogo.jsx';
 import { Nav } from '../Shared/Nav.jsx';
 import { Footer } from '../Shared/Footer.jsx';
 import { MenuSettings} from '../UserProfile/MenuSettings';
 import { PersonalInfo } from '../UserProfile/PersonalInfo';
-import { SmallBtn } from '../Buttons/SmallBtn'
+import { SmallBtn } from '../Buttons/SmallBtn';
+import { ReviewsContainer } from '../UserProfile/ReviewsContainer';
+import { LanguageSelection } from '../Shared/LanguageSelection';
 import './UserProfile.css';
 
 export const UserProfile = ( { userType } ) => {
+    const [page, setPage] = useState('lang');
+
     return (
         <div className="student-home-container">
             <div className="nav-bar-container">
@@ -28,10 +32,69 @@ export const UserProfile = ( { userType } ) => {
             </div>
             <PersonalInfo/>
             <div className="smallbtn2">
-              <SmallBtn label={'Submit'}/>
+            <SmallBtn label={'Submit'}/>
             </div>
             </div>
             <Footer />
         </div>
     )
+
+    if (page === 'lang') {
+        return (
+            <div className="student-home-container">
+                <div className="nav-bar-container">
+                    <div className="nav-logo">
+                        <TransparentLogo />
+                    </div>
+                    <div className="nav-links">
+                        <Nav />
+                    </div>
+                </div>
+                <div className="flex justify-center ml-80">
+                Language Selection
+                </div>
+                <div></div>
+                <div className="flex gap-20">
+                <MenuSettings/>
+                <div className="flex justify-around mt-5">
+                </div>
+                <div className="container flex justify-around">
+                <LanguageSelection languages={["English", "Spanish", "Japanese", "German", "French", "Korean"]}/>
+                <div className="smallbtn">
+                <SmallBtn label={'Submit'}/>
+                </div>
+                </div>
+                </div>
+                <Footer />
+            </div>
+        )
+    }
+
+    if (page === 'reviews') {
+        return (
+            <div className="student-home-container">
+                <div className="nav-bar-container">
+                    <div className="nav-logo">
+                        <TransparentLogo />
+                    </div>
+                    <div className="nav-links">
+                        <Nav />
+                    </div>
+                </div>
+                <div className="flex justify-center ml-80">
+                Reviews
+                </div>
+                <div></div>
+                <div className="flex gap-20">
+                <MenuSettings/>
+                <div className="flex justify-around mt-5">
+                </div>
+                <div className="reviews">
+                    <ReviewsContainer />
+                </div>
+                </div>
+                <Footer />
+            </div>
+        )
+    }
 }
