7a35c852711feede48395dd2a350d0de46d0872c
Cody Haines <chaines51@gmail.com>
2021-04-08 13:14:46 -0500

    Remove unnecessary import

--- react/src/components/Shared/Nav.jsx
+++ react/src/components/Shared/Nav.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { AuthContext, logout } from '../../auth';
 
 export const Nav = () => {
