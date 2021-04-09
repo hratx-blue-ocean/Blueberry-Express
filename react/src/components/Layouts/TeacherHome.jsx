fc6f55a807a30b53cbd9ab5607240a8768df764f
Tahsin Ahmed <tahsinahmed95@gmail.com>
2021-04-09 19:57:56 -0400

    added teacher name

--- react/src/components/Layouts/TeacherHome.jsx
+++ react/src/components/Layouts/TeacherHome.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useConext, useState } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { TransparentLogo } from '../Shared/TransparentLogo.jsx';
 import { Nav } from '../Shared/Nav.jsx';
 import { TeacherAppointmentContainer } from '../TeacherOnly/TeacherAppointmentContainer.jsx';
@@ -6,8 +6,10 @@ import { TeacherMessageContainer } from '../Shared/TeacherMessageContainer.jsx';
 import { Footer } from '../Shared/Footer.jsx';
 import './StudentHome.css';
 import { fetchAllMessages } from '../../api.js';
+import { AuthContext } from '../../auth';
 
 export const TeacherHome = () => {
+  const context = useContext(AuthContext);
   const [teacherMessages, setTeacherMessages] = useState([]);
 
   useEffect(() => {
@@ -27,7 +29,7 @@ export const TeacherHome = () => {
           <Nav />
         </div>
       </div>
-      <h1 className="welcome-teacher">Welcome back, Teacher!</h1>
+      <h1 className="welcome-teacher">Welcome back, {context.user.name}!</h1>
       <div className="flex justify-around mt-5">
         <TeacherAppointmentContainer />
         <TeacherMessageContainer messages={teacherMessages}/>
