1857a76381692d81912de4a6e606317bcbbda437
Matt <matt.collins087@gmail.com>
2021-04-10 13:58:50 -0500

    Edited Profile CSS

--- react/src/components/UserProfile/MenuSettings.jsx
+++ react/src/components/UserProfile/MenuSettings.jsx
@@ -5,7 +5,7 @@ import { SetAvailabilityModal } from '../Modals/SetAvailabilityModal';
 export const MenuSettings = ({ userType, action, curPage }) => {
   return (
     <div className="menu-settings-container">
-      <h1 className="text-white underline text-xl mt-4 mb-6">Settings</h1>
+      <h1 className="text-white underline text-xl mt-4 mb-8">Settings</h1>
       <div className="menu-container mb-4">
         <div className="mb-7">
           <SettingsBtn label="Personal Info" handleClick={action} />
