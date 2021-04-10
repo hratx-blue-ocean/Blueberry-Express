78fda3168d2595f2ded3fec46e7791c3137f777a
Tahsin Ahmed <tahsinahmed95@gmail.com>
2021-04-10 13:17:59 -0400

    added new logo kate made and more styling

--- react/src/components/Shared/TransparentLogo.jsx
+++ react/src/components/Shared/TransparentLogo.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 export const TransparentLogo = () => (
-  <div className="flex items-center nav-image">
-    <img className="logo-image" src='assets/transparent-logo.png'/>
-    <p className="company-name text-white ">Blueberry Express</p>
-  </div>
+  <Link to="/">
+    <div className="flex items-center nav-image">
+      <img className="logo-image" src='assets/transparent_logo_2.png'/>
+      <p className="company-name text-white -ml-4">Blueberry Express</p>
+    </div>
+  </Link>
+
   //
 );
